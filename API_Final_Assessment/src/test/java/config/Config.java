ca6a38a0211844242eac91a489bedb186398f187
Saitejakodi <299172@ust.com>
2026-07-21 15:44:07 +0530

    Submitting Final Assessment

--- API_Final_Assessment/src/test/java/config/Config.java
+++ API_Final_Assessment/src/test/java/config/Config.java
@@ -4,10 +4,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
 
-    public static final String BASE_URL = dotenv.get("BASE_URL");
+    public static final String BASE_URL =
+            System.getenv("BASE_URL") != null
+                    ? System.getenv("BASE_URL")
+                    : dotenv.get("BASE_URL");
 
-    private Config() {
-    }
+    private Config() {}
 }
