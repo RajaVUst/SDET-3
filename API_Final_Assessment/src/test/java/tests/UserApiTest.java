957ff6174bef1a3fe9fbe5d3c02c3f3edc9ea9ad
Saitejakodi <299172@ust.com>
2026-07-21 15:53:19 +0530

    retryyyyyyyyyyyyy

--- API_Final_Assessment/src/test/java/tests/UserApiTest.java
+++ API_Final_Assessment/src/test/java/tests/UserApiTest.java
@@ -28,7 +28,7 @@ public class UserApiTest extends BaseTest {
     void createUserGenerateTokenAndGetBooks() {
 
         User user = new User(
-                "Saiteja123",
+                "Saitejakodi123",
                 "Password@123"
         );
 
